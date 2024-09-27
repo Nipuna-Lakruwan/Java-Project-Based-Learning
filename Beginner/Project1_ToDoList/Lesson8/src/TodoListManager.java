92c2c0ed0d1defe26bd0d17ed9ff613e65046caf
Nipuna Lakruwan <laknipsa@gmail.com>
2024-09-27 18:33:59 +0530

    ADD: Lesson 8: Exception Handling in Java

---
+++ Beginner/Project1_ToDoList/Lesson8/src/TodoListManager.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class TodoListManager<T> {
+    private ArrayList<T> tasks;
+
+    public TodoListManager() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(T task) {
+        tasks.add(task);
+        System.out.println("Task added: " + task);
+    }
+
+    public void viewTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks in the list.");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+
+    public void removeTask(int index) throws TaskNotFoundException {
+        if (index < 1 || index > tasks.size()) {
+            throw new TaskNotFoundException("Invalid task number.");
+        }
+        System.out.println("Task removed: " + tasks.get(index - 1));
+        tasks.remove(index - 1);
+    }
+
+    public void completeTask(int index) {
+        if (index >= 1 && index <= tasks.size()) {
+            System.out.println("Task completed: " + tasks.get(index - 1));
+            tasks.remove(index - 1);
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+}
